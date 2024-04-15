0c18fdbe4b65d044d84997dd473bf4a5ae26c65b
KankichiJibiki <kan.0408@outlook.jp>
2024-04-16 06:15:58 +0900

    Text to speech added

--- Wordpicker_API/Services/TextToSpeechService/ITextToSpeechService.cs
+++ Wordpicker_API/Services/TextToSpeechService/ITextToSpeechService.cs
@@ -1,6 +1,9 @@
-﻿namespace Wordpicker_API.Services.TextToSpeechService
+﻿using Wordpicker_API.Utils;
+
+namespace Wordpicker_API.Services.TextToSpeechService
 {
     public interface ITextToSpeechService
     {
+        Task<ApiResponse> ConvertTextToSpeech(string text);
     }
 }
--- Wordpicker_API/Services/TextToSpeechService/TextToSpeechService.cs
+++ Wordpicker_API/Services/TextToSpeechService/TextToSpeechService.cs
@@ -1,7 +1,26 @@
-﻿namespace Wordpicker_API.Services.TextToSpeechService
-{
-    public class TextToSpeechService: ITextToSpeechService
-    {
+﻿using Wordpicker_API.Utils;
 
+namespace Wordpicker_API.Services.TextToSpeechService
+{
+    public class TextToSpeechService : ITextToSpeechService
+    {
+        private readonly IConfiguration _config;
+        private readonly ApiResponse _response;
+
+        public TextToSpeechService(IConfiguration config) 
+        {
+            _config = config;
+            _response = new ApiResponse();
+        }
+        public async Task<ApiResponse> ConvertTextToSpeech(string text)
+        {
+            if(string.IsNullOrEmpty(text))
+            {
+                _response.SetResponse(false, StatusCodes.Status400BadRequest, "Text was either null or empty", "");
+                return _response;
+            }
+
+
+        }
     }
 }
