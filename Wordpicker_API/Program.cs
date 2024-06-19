4ebb9fd2739bebf9277573cb8c8b6c6d0a8d74cb
KankichiJibiki <kan.0408@outlook.jp>
2024-06-20 07:10:51 +0900

    set up a feature that switches config files for dev and prod

--- Wordpicker_API/Program.cs
+++ Wordpicker_API/Program.cs
@@ -9,6 +9,13 @@ namespace Wordpicker_API
 
         public static IHostBuilder CreateHostbuilder(string[] args) => 
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
